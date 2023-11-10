71e5e03e9d8fca57028714b790c5960f0af92ccf
Joel De Ariovaldo <54207437+JoelDeAriovaldo@users.noreply.github.com>
2023-11-10 09:01:12 +0000

    trabalhando para mostrar decks main page

--- flashcardsage/server/src/index.ts
+++ flashcardsage/server/src/index.ts
@@ -1,13 +1,41 @@
-import express, { Request, Response} from "express";
+ import { config } from "dotenv";
+config();
+
+import express, { Request, Response } from "express";
 import mongoose from "mongoose";
+import cors from "cors";
+import Deck from "./models/Deck";
+
+const PORT = 5000;
+
 
 const app = express();
+app.use(cors({
+    origin: "*",
+  })
+);
 
-const db = await mongoose.connect(
-    'mongodb+srv://jproariovald:2pLvp3rn36w8mhP9@joel0.nqcaxwg.mongodb.net/?retryWrites=true&w=majority');
-
-app.get("/", (req: Request, res: Response) => {
-    res.send("Ola mundo!");
+app.get("/decks", async (req: Request, res: Response) => {
+  //TODO: buscar todos os decks e enviar de volta ao usuário
+  //1.como buscamos os decks de mongo?
+  const decks = await Deck.find();
+  //2.como enviamos de volta o array para a interface do usuário?
+  res.json(decks);
 });
 
-app.listen(5000);
+app.get("/decks", async (req: Request, res: Response) => {
+  const newDeck = new Deck({
+    title: req.body.title,
+  });
+  const createdDeck = await newDeck.save();
+  res.json(createdDeck);
+});
+
+mongoose
+  .connect(process.env.MONGO_URL ?? "").then(() => {
+    console.log(`Listening on port ${PORT}`);
+    app.listen(PORT);
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
