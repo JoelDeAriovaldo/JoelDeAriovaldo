3e608ec8ed10017d0e55cf816d5285bb69f6abfe
Joel De Ariovaldo Romao <joeldeariovaldodevef@gmail.com>
2023-11-14 13:54:56 +0000

    import async page

--- react-weather-app/src/components/search/search.js
+++ react-weather-app/src/components/search/search.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import ReactSelectAsyncPaginate from 'react-select-async-paginate';
 import { geoApiOptions, GEO_API_URL } from "../../api";
 
 const Search = ({ onSearchChange }) => {
